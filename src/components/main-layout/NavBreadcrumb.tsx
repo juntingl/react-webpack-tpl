a775452e23e911aba3fa2a8067f03862a3eb3cae
Junting <342766475@qq.com>
2022-11-02 15:48:57 +0800

    feat: breadcrumb

--- src/components/main-layout/NavBreadcrumb.tsx
+++ src/components/main-layout/NavBreadcrumb.tsx
@@ -1,18 +1,26 @@
 import { Breadcrumb } from "antd";
+import { Link, useLocation } from "react-router-dom";
+
+import { getActiveRoutes } from "@/router";
 
 const NavBreadcrumb = () => {
-  return (
-    <Breadcrumb>
-      <Breadcrumb.Item>Home</Breadcrumb.Item>
-      <Breadcrumb.Item>
-        <a href="">Application Center</a>
-      </Breadcrumb.Item>
-      <Breadcrumb.Item>
-        <a href="">Application List</a>
-      </Breadcrumb.Item>
-      <Breadcrumb.Item>An Application</Breadcrumb.Item>
-    </Breadcrumb>
-  );
+  const location = useLocation();
+
+  const activeRoutes = getActiveRoutes(location.pathname);
+
+  const breadcrumbItems = activeRoutes
+    .filter((route) => route.title)
+    .map((route, i) => {
+      const isFirstOrLastIndex = i === activeRoutes.length - 1 || i === 0;
+
+      return (
+        <Breadcrumb.Item key={route.path}>
+          {isFirstOrLastIndex ? route.title : <Link to={route.path}>{route.title}</Link>}
+        </Breadcrumb.Item>
+      );
+    });
+
+  return <Breadcrumb>{breadcrumbItems}</Breadcrumb>;
 };
 
 export default NavBreadcrumb;
